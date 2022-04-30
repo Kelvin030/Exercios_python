a1f03bc9f9fe3cdf177884426f84071b420c341d
kelvin <silva.com.br123007@gmail.com>
2022-04-30 12:19:58 -0300

    complete

--- exercios/ex090.py
+++ exercios/ex090.py
@@ -1,2 +1,12 @@
-nome = "kelvin"
-print("hello world")
+from ast import alias
+
+
+aluno = {}
+aluno['nome'] = str(input("Nome: "))
+aluno['nota'] = float(input('Nota Medía: '))
+print(f"O nome do aluno é {aluno['nome']}")
+print(f"A nota é {aluno['nota']}")
+if aluno['nota'] >= 7:
+    print("O aluno foi aprovado!!!")
+else:
+    print("O aluno foi reprovado!!!")
