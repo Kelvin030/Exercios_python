dda701ba53483880254c067e6d119a1574058a6f
kelvin <silva.com.br123007@gmail.com>
2022-04-30 13:07:27 -0300

    não acabado

--- exercios/ex091.py
+++ exercios/ex091.py
@@ -1,7 +1,16 @@
 from random import randint
+test = []
+cont = 1
 jogadores = {
-    'jogador1':randint(1,6),
-    'jogador2':randint(1,6),
-    'jogador3':randint(1,6),
-    'jogador4':randint(1,6),
+    'jogador 1':randint(1,6),
+    'jogador 2':randint(1,6),
+    'jogador 3':randint(1,6),
+    'jogador 4':randint(1,6),
 }
+for key, value in jogadores.items():
+    print(f'O {key}, tirou {value}')
+
+print()
+print(" Ranking dos jogadores ".center(30,"="))
+print()
+
