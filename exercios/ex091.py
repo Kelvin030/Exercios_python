8e7ed046726d430b198ba9852b251f18427f3f1c
kelvin <silva.com.br123007@gmail.com>
2022-05-01 13:49:33 -0300

    ex091_finished

--- exercios/ex091.py
+++ exercios/ex091.py
@@ -1,15 +1,24 @@
 from random import randint
+from time import sleep
+from operator import itemgetter
 test = []
 cont = 1
 jogadores = {
-    'jogador1':randint(1,6),
-    'jogador2':randint(1,6),
-    'jogador3':randint(1,6),
-    'jogador4':randint(1,6),
+    'Jogador1':randint(1,6),
+    'Jogador2':randint(1,6),
+    'Jogador3':randint(1,6),
+    'Jogador4':randint(1,6),
 }
 for key, value in jogadores.items():
     print(f'O {key}, tirou {value}')
+    sleep(0.5)
 
 print()
 print(" Ranking dos jogadores ".center(30,"="))
 print()
+
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+for c, v in enumerate(ranking):
+    print(f'{c+1}º Lugar: {v[0]} com {v[1]} pontos')
+    sleep(1)
+
