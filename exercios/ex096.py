45a01f6b7fbd2218284daf670074c189014d377d
kelvin <silva.com.br123007@gmail.com>
2022-05-06 16:21:29 -0300

    finished

--- exercios/ex096.py
+++ exercios/ex096.py
@@ -2,6 +2,7 @@ def area(lagura, comprimento):
     area = lagura * comprimento
     print(f"A área de {lagura} X {comprimento} é de {area}m²")
 
+
 print("  Controle de Terrenos")
 print("-" * 25)
 lagura = float(input("Lagura (m): "))
