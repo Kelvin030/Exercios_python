2e2448000f4d917d78358a3cb736935371a8dea3
kelvin <silva.com.br123007@gmail.com>
2022-05-07 11:29:34 -0300

    finished

--- exercios/ex097.py
+++ exercios/ex097.py
@@ -1,7 +1,8 @@
 def escreva(msg, caractere):
-    print(caractere * (len(msg) + 6))
-    print(f"{msg}".center(len(msg) + 6))
-    print(caractere * (len(msg) + 6))
+
+    print(caractere * (len(msg) + 4))
+    print(f"{msg}".center(len(msg) + 4))
+    print(caractere * (len(msg) + 4))
 
 
 # Programa Principal
