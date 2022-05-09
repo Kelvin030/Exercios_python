8184112353e6912c66138c392ec719daa26c5e02
kelvin <silva.com.br123007@gmail.com>
2022-05-09 19:59:32 -0300

    finished

--- exercios/ex105.py
+++ exercios/ex105.py
@@ -1,4 +1,10 @@
 def notas(*n, sit=False):
+    """
+    --> Função para analizar nota e situação de um aluno(a)
+    :param n: Uma ou mais notas do aluno(a)
+    :param sit: Valor opcional indicando a situação do aluno(a)
+    :return: Dicionario com varias informaçoes do aluno(a)
+    """
     situação = dict()
 
     situação['total'] = len(n)
@@ -12,7 +18,7 @@ def notas(*n, sit=False):
             situação['situação'] = 'RAZOÁVEL'
         elif situação['media'] >= 8:
             situação['situação'] = 'BOM'
-    print(situação)
+    return situação
 
 
 # Programa Principal
