f9ae1db41320f3d4871085f7b718dad75d0bfff5
Kelvin <silva.com.br123007@gmail.com>
2022-05-21 15:52:54 -0300

    finished

--- exercios/ex118.py
+++ exercios/ex118.py
@@ -1,1 +1,30 @@
-print('Hello World')
+#Primeiro programa com API
+import requests
+from json import loads
+
+def requisição_cep(cep):
+    url = requests.get(f'https://cep.awesomeapi.com.br/JSON/{cep}')
+    requisição = loads(url.text)
+    try:
+        print(f'\nEndereço: {requisição["address"]}')
+    except:
+        print(f'Erro: {requisição["status"]}, {requisição["message"]}')
+    else:
+        print(f'Cidade: {requisição["city"]}')
+        print(f'Distrito: {requisição["district"]}')
+        print(f'Estado: {requisição["state"]}')
+        print(f'DDD: {requisição["ddd"]}')
+
+
+print('=' * 40)
+print('Verificador de CEP'.center(40))
+print('=' * 40)
+while True:
+    try:
+        cep = int(input("Digite um cep (Somente numeros): "))
+    except:
+        print('Erro! tente novamente...')
+    else:
+        break
+
+requisição_cep(cep)
