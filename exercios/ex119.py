d9798bbe92b79ce1e83115f77d6a0fc7afdab56d
Kelvin <silva.com.br123007@gmail.com>
2022-05-22 16:13:30 -0300

    finished

---
+++ exercios/ex119.py
@@ -0,0 +1,57 @@
+def requisição(moeda1='USD'):
+    from requests import get
+    from json import loads
+    try:
+        url = get(f'https://economia.awesomeapi.com.br/json/last/{moeda1}-BRL')
+    except Exception as erro:
+        print(f'Ocorreu o erro: {erro}')
+    else:
+        return loads(url.text)
+
+
+def BTCBRL(dic):
+    print('COTAÇÃO: R${}'.format(dic["BTCBRL"]["low"]))
+    print('DATA: {}'.format(dic["BTCBRL"]["create_date"]))
+
+
+def USDBRL(dic)
+    print('COTAÇÃO: R${}'.format(dic["USDBRL"]["low"]))
+    print('DATA: {}'.format(dic["USDBRL"]["create_date"]))
+
+
+def EURBRL(dic):
+    print('COTAÇÃO: R${}'.format(dic["EURBRL"]["low"]))
+    print('DATA: {}'.format(dic["EURBRL"]["create_date"]))
+
+
+print('=' * 40)
+print('Cotação Moedas'.center(40))
+print('=' * 40)
+print('Deseja ver a cotação de qual moeda:')
+print('1 - USD')
+print('2 - BTC')
+print('3 - EUR')
+print('4 - sair')
+print('=' * 40)
+while True:
+    while True:
+        try:
+            escolha = int(input('Sua opcção: '))
+        except:
+            print("erro tente novamente...")
+        else:
+            break
+    if escolha == 1:
+        USDBRL(requisição('USD'))
+        break
+    elif escolha == 2:
+        BTCBRL(requisição('BTC'))
+        break
+    elif escolha == 3:
+        EURBRL(requisição('EUR'))
+        break
+    elif escolha == 4:
+        print('Volte sempre!!!')
+        break
+    else:
+        print('opção invalida...')
